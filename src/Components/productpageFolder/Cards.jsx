62d67d9f62be54b0fbdcc04ae790eb9bbf6de68c
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-06-20 16:07:12 +0100

    Changes were made to Cards component

--- src/Components/productpageFolder/Cards.jsx
+++ src/Components/productpageFolder/Cards.jsx
@@ -9,14 +9,13 @@ const Cards = () => {
 
     return (
         <div className="container mx-auto p-4">
-            <CardBody className="flex flex-wrap">
+            <CardBody className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                 {products.map(product => (
-                    <Card key={product.id} className="product-card w-60 h-auto p-3 bg-gray-50 flex flex-col gap-1 rounded-2xl m-2">
+                    <Card key={product.id} className="product-card w-full h-auto p-3 bg-gray-50 flex flex-col gap-1 rounded-2xl m-2">
                         <div className="h-48 bg-gray-700 rounded-xl"></div>
                         <div className='flex flex-row justify-between px-3 mt-5 mb-5'>
-
                             <span className="text-xl text-black font-berkshire">{product.name}</span>
-                            <p className="font-bold  text-red-600">₦{product.price}</p>
+                            <p className="font-bold text-red-600">₦{product.price}</p>
                         </div>
                         <Link to={`/check-out-product/${product.id}`}>
                             <button
